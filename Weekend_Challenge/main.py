9158ff9ff6bd64918133ffc68048f6af13b5ac29
Arnav Sharma <arnav05in@gmail.com>
2026-05-16 15:30:52 +0100

    Added readers

--- Weekend_Challenge/main.py
+++ Weekend_Challenge/main.py
@@ -1,6 +1,7 @@
 import csv
-import wave
+from scipy.io import wavfile
 from FIR import FIR
+import os
 
 def run():
     # Prepare FIR
@@ -9,7 +10,16 @@ def run():
         reader = csv.reader(file)
         for row in reader:  
             filter_coefs.append(float(row[0]))
+    absolute_path = os.path.dirname(__file__)
+    relative_path = 'wav_files'
+    path = os.path.join(absolute_path, relative_path)
+    files = os.listdir(path)
+    for file in files:
+        data_rate, data = wavfile.read(os.path.join(path,file))
+        break
+    
+    
+    
 
-    filter = FIR(filter_coefs)
     
 run()
